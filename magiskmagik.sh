ac7a94af4e9857580f14f5a15549d8001760d43a
Jon West <electrikjesus@gmail.com>
2019-05-09 18:33:24 -0400

    Add magisk from
    https://github.com/Black-Seraph/android_patches_gpdwin

---
+++ magiskmagik.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# determine the GPD Win patches folder location
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# bundle MagiskManager into the system
+cd "out/target/product/x86_64/system/app"
+mkdir MagiskManager
+cd MagiskManager
+cp -f "$DIR/magisk/MagiskManager.apk" .
+mkdir tmp
+cd tmp
+unzip ../MagiskManager.apk
+mv lib ..
+cd ..
+rm -rf tmp
+cd ..
+
+cd ../../../../../../..
+
+# delete all files we wish to rebuild
+rm -f "out/target/product/x86_64/*.iso"
+rm -f "out/target/product/x86_64/system.sfs"
+rm -f "out/target/product/x86_64/ramdisk.img"
+rm -f "out/target/product/x86_64/obj/PACKAGING/systemimage_intermediates/system.img"
+
+# copy the magisk init binary into the ramdisk
+# we need to go this roundabout way because android-x86 has three ramdisks...
+# but we must only patch the android-x86 live system one, otherwise we bootloop!
+# cd "out/target/product/x86_64"
+# mkdir magisk
+# cd magisk
+# gzip -cd ../ramdisk.img | cpio -i
+# cp "$DIR/magisk/init" .
+# find . | cpio -o -H newc | gzip > ../ramdisk.img
+# cd ..
+# rm -rf magisk
+# cd ../../../..
+cd "bootable/newinstaller/initrd/bin"
+cp "$DIR/magisk/init" .
+cd ../../../..
+
+# workaround until I figure out what's wrong with the code above
+# cp "$DIR/magisk/ramdisk.img" "out/target/product/x86_64"
+
+# setup the aosp build environment
+# . build/envsetup.sh
+
+# pick our lunch
+# lunch android_x86_64-userdebug
+
+# rebuild the image
+# m -j4 iso_img
